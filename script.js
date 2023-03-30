ad4e8b8b656ffc6afd3dfbd5f795221fa4761139
Amr Hefny <ahefny1987@gmail.com>
2023-03-31 00:07:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let act = document.getElementById("text")
+
+act.addEventListener("click", () =>{
+    let penguin = document.getElementById("penguin")
+    let message = document.getElementById("message")
+    penguin.classList.remove("off")
+    penguin.classList.add("appear")
+    setTimeout(()=>{
+        penguin.classList.remove("appear")
+        penguin.classList.add("float")
+    }, 10000)
+    
+    setTimeout(()=>{
+        message.classList.add("fade-in")
+        
+    },11000)
+    act.classList.add("off")
+})
+
+
+
+
